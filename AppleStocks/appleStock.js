5ec76adc8c34c7d6a29181a3de877bd0e0e2fa2d
persiden <persiden.smith@gmail.com>
2016-02-16 16:37:03 -0500

    code after tips

--- AppleStocks/appleStock.js
+++ AppleStocks/appleStock.js
@@ -1,11 +1,10 @@
 var stock_prices_yesterday = [10, 7, 5, 4, 3, 2, 1];
-							//change to any value works will all numbers
 
 function get_max_profit(stock_prices_yesterday){
-	var maxProfit = -Number.MAX_VALUE;
-	var minVal  = Number.MAX_VALUE;
-	var maxVal = -Number.MAX_VALUE;
-	var curVal  = 0;
+	var maxProfit 	= -Number.MAX_VALUE;
+	var minVal  	= Number.MAX_VALUE;
+	var maxVal 		= -Number.MAX_VALUE;
+	var curVal  	= 0;
 	for (i = 0; i < stock_prices_yesterday.length;i++){
 		curVal = stock_prices_yesterday[i];
 		if (i === 0){
