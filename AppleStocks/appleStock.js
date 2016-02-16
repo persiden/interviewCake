b1d89e49286247930a207e49b435163ea3acf739
persiden <persiden.smith@gmail.com>
2016-02-16 16:40:50 -0500

    code after tips

--- AppleStocks/appleStock.js
+++ AppleStocks/appleStock.js
@@ -1,17 +1,14 @@
 var stock_prices_yesterday = [10, 7, 5, 4, 3, 2, 1];
 
 function get_max_profit(stock_prices_yesterday){
-	var maxProfit 	= -Number.MAX_VALUE;
-	var minVal  	= Number.MAX_VALUE;
-	var maxVal 		= -Number.MAX_VALUE;
-	var curVal  	= 0;
+	var maxProfit = -Number.MAX_VALUE;
+	var minVal = Number.MAX_VALUE;
 	for (i = 0; i < stock_prices_yesterday.length;i++){
-		curVal = stock_prices_yesterday[i];
 		if (i === 0){
 			minVal = stock_prices_yesterday[i]
 		}else{
-			maxProfit = Math.max(maxProfit,(curVal-minVal));
-			minVal = Math.min(minVal,curVal);
+			maxProfit = Math.max(maxProfit,(stock_prices_yesterday[i]-minVal));
+			minVal = Math.min(minVal,stock_prices_yesterday[i]);
 		}
 	}
 	return maxProfit
